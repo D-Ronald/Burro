b9e9ec33ff266012cce2205470760e49f2cfc5e9
Daniel Ronald <96017424+D-Ronald@users.noreply.github.com>
2022-06-16 16:49:19 -0300

    comit inicial

---
+++ script.js
@@ -0,0 +1,22 @@
+var a =0
+var texto = document.querySelector('#texto');
+var pai = document.querySelector('#pai');
+var sim = document.querySelector('#yes');
+var n = document.querySelector('#paiNo');
+
+function yes(){
+    pai.removeChild(sim);
+    pai.removeChild(n);
+    texto.innerHTML = 'Eu sabia!';
+}
+function no(){
+    a =a+1
+    if (a%2==1){
+        document.getElementById('no').style.marginLeft = '10px'
+        document.getElementById('yes').style.marginLeft = '10px'
+    }
+    if (a%2==0){
+        document.getElementById('no').style.marginLeft = '50px'
+        document.getElementById('yes').style.marginLeft = '50px'
+    }
+}
